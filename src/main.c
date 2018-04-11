1f9eb01805d50f735293de6fd5b9b597e117b025
tdfirth <tom@stockopedia.com>
2018-04-11 20:24:29 +0100

    Basic calculator implemented... looking good

--- src/main.c
+++ src/main.c
@@ -8,9 +8,24 @@ int main(int argc, const char *argv[])
   init_vm();
   Chunk chunk;
   init_chunk(&chunk);
+
   int constant = add_constant(&chunk, 1.2);
   write_chunk(&chunk, OP_CONSTANT, 123);
   write_chunk(&chunk, constant, 123);
+
+  constant = add_constant(&chunk, 3.4);
+  write_chunk(&chunk, OP_CONSTANT, 123);
+  write_chunk(&chunk, constant, 123);
+
+  write_chunk(&chunk, OP_ADD, 123);
+
+  constant = add_constant(&chunk, 5.6);
+  write_chunk(&chunk, OP_CONSTANT, 123);
+  write_chunk(&chunk, constant, 123);
+
+  write_chunk(&chunk, OP_DIVIDE, 123);
+  write_chunk(&chunk, OP_NEGATE, 123);
+
   write_chunk(&chunk, OP_RETURN, 123);
   disassemble_chunk(&chunk, "test chunk");
   interpret(&chunk);
--- src/vm.h
+++ src/vm.h
@@ -2,10 +2,15 @@
 #define clox_vm_h
 
 #include "chunk.h"
+#include "value.h"
+
+#define STACK_MAX 256
 
 typedef struct {
   Chunk *chunk;
   uint8_t *ip;
+  Value stack[STACK_MAX];
+  Value *stack_top;
 } VM;
 
 typedef enum {
@@ -17,5 +22,7 @@ typedef enum {
 void init_vm();
 void free_vm();
 InterpretResult interpret(Chunk *chunk);
+void push(Value value);
+Value pop();
 
 #endif
